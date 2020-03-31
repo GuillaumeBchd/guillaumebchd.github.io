0be20016e0a632840d30aa166c0aa19969a35aee
GuillaumeBchd <guillaume.bouchard@etu.univ-tours.fr>
2020-03-31 17:26:25 +0200

    Updates

---
+++ precache-manifest.a608341e66ca761d3dd26a924b0d48cb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c00aa1d05beae61b3db359b899742f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6cf8ff44e07c6ef778c",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "513261c65e688f750feb",
+    "url": "/static/css/main.c9739bb9.chunk.css"
+  },
+  {
+    "revision": "c6cf8ff44e07c6ef778c",
+    "url": "/static/js/2.7efe9f04.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.7efe9f04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "513261c65e688f750feb",
+    "url": "/static/js/main.97abefb6.chunk.js"
+  },
+  {
+    "revision": "7887510fd9cc69aa5c15",
+    "url": "/static/js/runtime-main.071d9139.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2fbd8975262ddb819ef2ce6182aad731.js"
+  "/precache-manifest.a608341e66ca761d3dd26a924b0d48cb.js"
 );
 
 self.addEventListener('message', (event) => {
